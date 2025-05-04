651d5dd8f2c1de29c99289f44b9b9aeabf4ee7cf
Jason Cairns <jason@cair.nz>
2025-05-04 13:21:20 +1200

    feat: add python

--- flake.nix
+++ flake.nix
@@ -5,12 +5,33 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     self.submodules = true;
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       nixpkgs,
       flake-utils,
+      uv2nix,
+      pyproject-nix,
+      pyproject-build-systems,
+
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -18,6 +39,8 @@
 
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        # R Environment
 
         orcv = pkgs.rPackages.buildRPackage {
           name = "orcv";
@@ -66,6 +89,28 @@
         prodREnv = pkgs.rWrapper.override { packages = Rpackages; };
         devREnv = pkgs.rWrapper.override { packages = Rpackages ++ [ pkgs.rPackages.languageserver ]; };
 
+        # Python Environment
+
+        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./largescaler; };
+
+        overlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "wheel"; # or sourcePreference = "sdist";
+        };
+        python = pkgs.python313;
+        pythonSet =
+          (pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          }).overrideScope
+            (
+              pkgs.lib.composeManyExtensions [
+                pyproject-build-systems.overlays.default
+                overlay
+              ]
+            );
+        largescaler = pythonSet.mkVirtualEnv "largescaler-env" workspace.deps.default;
+
+        # Container
+
         baseImage = pkgs.dockerTools.pullImage {
           imageName = "ubuntu";
           imageDigest = "sha256:1e622c5f073b4f6bfad6632f2616c7f59ef256e96fe78bf6a595d1dc4376ac02";
@@ -85,6 +130,7 @@
               pkgs.zellij
               pkgs.bash
               prodREnv
+              largescaler
             ];
             pathsToLink = [
               "/bin"
@@ -101,14 +147,31 @@
       {
         packages.default = container;
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            devREnv
-            clang
-            clang-tools
-            zellij
-          ];
-        };
+        devShells.default =
+          let
+            largescaler-dev-venv = pythonSet.mkVirtualEnv "largescaler-dev-env" workspace.deps.all;
+          in
+          pkgs.mkShell {
+            buildInputs = with pkgs; [
+              clang
+              clang-tools
+              zellij
+              devREnv
+              largescaler-dev-venv
+              pkgs.uv
+            ];
+            env = {
+              UV_NO_SYNC = "1";
+              UV_PYTHON = "${largescaler-dev-venv}/bin/python";
+              UV_PYTHON_DOWNLOADS = "never";
+            };
+
+            shellHook = ''
+              unset PYTHONPATH
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
+            '';
+
+          };
       }
     );
 }
